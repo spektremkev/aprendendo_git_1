b757cbfda570c222794e2a800359fe926cda421b
Gabriel Martins <gabrieldossnmartins@outlook.com>
2021-03-30 15:09:33 -0300

    primeiro commit de teste

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
