26c4a512f74924beef5f12df7342b87c1c2050f4
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2021-04-01 14:52:41 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Aula_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("testendo push");
+            Console.WriteLine("testando push");
         }
     }
 }
